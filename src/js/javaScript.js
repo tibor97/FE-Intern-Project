295b478298537f01e41a0ff5550994faa726e0a9
tibor97 <tibor.serencei_vuk@yahoo.com>
2020-01-18 21:41:16 +0100

    nav

--- src/js/javaScript.js
+++ src/js/javaScript.js
@@ -1,1 +1,29 @@
+$(document).ready(function(){
 
+	$('a').click(function(e){
+	    var jump = $(this).attr('href');
+	    var new_position = $(jump).offset();
+	    $('html, body').stop().animate({ scrollTop: new_position.top }, 500);
+	    e.preventDefault();
+	});
+
+
+	$.fn.isInViewport = function() {
+	  var elementTop = $(this).offset().top;
+	  var elementBottom = elementTop + $(this).outerHeight();
+
+	  var viewportTop = $(window).scrollTop();
+	  var viewportBottom = viewportTop + $(window).height();
+
+	  return elementBottom > viewportTop && elementTop < viewportBottom;
+	};
+	$(window).on('resize scroll', function() {
+		if($("#aboutUs2").isInViewport()||$("#advisors2").isInViewport()){
+	    	$('a').css('color', '#12202d');
+		    }else{
+		    	$('a').css('color', 'white');
+		    }
+	});
+
+
+});
