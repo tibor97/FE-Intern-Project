0a8be4f004e2845812d1f88c6c6e55004fd3e4e9
tibor97 <tibor.serencei_vuk@yahoo.com>
2020-01-19 16:10:21 +0100

    log in form

--- src/js/javaScript.js
+++ src/js/javaScript.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
 
+	var modal = document.getElementById('logDiv');
+
+	window.onclick = function(event) {
+	    if (event.target == modal) {
+	        modal.style.display = "none";
+	    }
+	}
+	
 	$('a').click(function(e){
 	    var jump = $(this).attr('href');
 	    var new_position = $(jump).offset();
@@ -18,9 +26,19 @@ $(document).ready(function(){
 	};
 	$(window).on('resize scroll', function() {
 		if($("#aboutUs2").isInViewport()||$("#advisors2").isInViewport()){
-	    	$('a').css('color', '#12202d');
+	    	$('.nav a').css('color', '#12202d');
 		    }else{
-		    	$('a').css('color', 'white');
+		    	$('.nav a').css('color', 'white');
 		    }
 	});
 });
+
+// Get the modal
+var modal = document.getElementById('logDiv');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
