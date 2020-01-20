40d23e8bd815cb194bfe0bd7aa1bf28e8bfc3443
tibor97 <tibor.serencei_vuk@yahoo.com>
2020-01-20 23:11:49 +0100

    scrollToTop

--- src/js/javaScript.js
+++ src/js/javaScript.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
 	    $('html, body').stop().animate({ scrollTop: new_position.top }, 500);
 	    e.preventDefault();
 	});
-	
+
 
 	$.fn.isInViewport = function() {
 	  var elementTop = $(this).offset().top;
@@ -43,13 +43,10 @@ $(document).ready(function(){
 			$(".title h1").animate({ opacity:'1'}, 2000);
 			$(window).on('resize scroll', function() {
 				if($("#aboutUs2").isInViewport()){
-			    	$('.nav a').css('color', '#12202d');
 			    	$(".aboutUs").animate({ right:'0%'}, 1500);
+			    	$(".scrollToTop").css({ display:'block'}, 1500);
 				    }else if($("#advisors2").isInViewport()){
-			    		$('.nav a').css('color', '#12202d');
 			    		$(".advisorsCard").animate({ left:'0%'}, 1500);
-				    	}else{
-				    		$('.nav a').css('color', 'white');
 				    	}
 			});	
 		}
