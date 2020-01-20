9651d008c3ef76a29f693d89993203cf34af602d
tibor97 <tibor.serencei_vuk@yahoo.com>
2020-01-20 17:58:08 +0100

    animations

--- src/js/javaScript.js
+++ src/js/javaScript.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
 
 	var modal = document.getElementById('logDiv');
-
 	window.onclick = function(event) {
 	    if (event.target == modal) {
 	        modal.style.display = "none";
 	    }
 	}
+
 	
 	$('a').click(function(e){
 	    var jump = $(this).attr('href');
@@ -14,6 +14,7 @@ $(document).ready(function(){
 	    $('html, body').stop().animate({ scrollTop: new_position.top }, 500);
 	    e.preventDefault();
 	});
+	
 
 	$.fn.isInViewport = function() {
 	  var elementTop = $(this).offset().top;
@@ -24,21 +25,33 @@ $(document).ready(function(){
 
 	  return elementBottom > viewportTop && elementTop < viewportBottom;
 	};
-	$(window).on('resize scroll', function() {
-		if($("#aboutUs2").isInViewport()||$("#advisors2").isInViewport()){
-	    	$('.nav a').css('color', '#12202d');
-		    }else{
-		    	$('.nav a').css('color', 'white');
-		    }
-	});
+	if(window.innerWidth > 600){
+		$(".title img").animate({left: '14%'}, 1500);
+		$(".title h1").animate({ opacity:'1'}, 2000);
+		$(window).on('resize scroll', function() {
+			if($("#aboutUs2").isInViewport()){
+		    	$('.nav a').css('color', '#12202d');
+		    	$(".aboutUs").animate({ right:'0%'}, 1500);
+			    }else if($("#advisors2").isInViewport()){
+		    		$('.nav a').css('color', '#12202d');
+		    		$(".advisorsCard").animate({ left:'0%'}, 1500);
+			    	}else{
+			    		$('.nav a').css('color', 'white');
+			    	}
+		});	
+		}else{
+			$(".title h1").animate({ opacity:'1'}, 2000);
+			$(window).on('resize scroll', function() {
+				if($("#aboutUs2").isInViewport()){
+			    	$('.nav a').css('color', '#12202d');
+			    	$(".aboutUs").animate({ right:'0%'}, 1500);
+				    }else if($("#advisors2").isInViewport()){
+			    		$('.nav a').css('color', '#12202d');
+			    		$(".advisorsCard").animate({ left:'0%'}, 1500);
+				    	}else{
+				    		$('.nav a').css('color', 'white');
+				    	}
+			});	
+		}
 });
 
-// Get the modal
-var modal = document.getElementById('logDiv');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
